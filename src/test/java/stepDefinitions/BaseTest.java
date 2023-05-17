a0a639db9643d6018475ba7a785ce2fc782c56af
jude <quimiojude@gmail.com>
2023-05-17 12:11:47 +0800

    add hooks and screenshot

--- src/test/java/stepDefinitions/BaseTest.java
+++ src/test/java/stepDefinitions/BaseTest.java
@@ -1,7 +1,15 @@
-package Base;
+package stepDefinitions;
 
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 public class BaseTest {
 
@@ -15,10 +23,27 @@ public class BaseTest {
 
      private WebDriver driver;
 
-     @BeforeMethod
+     @Before
      public void startDriver(){
+          ChromeOptions options =  new ChromeOptions();
+          options.addArguments("--remote-allow-origins=*");
+          driver = new ChromeDriver(options);
+          setDriver(driver);
+     }
+     @After
+     public void quitDriver(){
+          getDriver().quit();
+     }
 
 
+     @AfterStep
+     public void addScreenshot(Scenario scenario){
+
+          //validate if scenario has failed
+          if(scenario.isFailed()) {
+               final byte[] screenshot = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.BYTES);
+               scenario.attach(screenshot, "image/png", "image");
+          }
      }
 
 }
