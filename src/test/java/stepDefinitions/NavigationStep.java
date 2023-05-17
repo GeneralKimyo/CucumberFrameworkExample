0d98c264f38da7a7eacf742e2824dd5228b50ccd
jude <quimiojude@gmail.com>
2023-05-17 12:14:48 +0800

    add additional navigation scenario and tags for testcaseID

--- src/test/java/stepDefinitions/NavigationStep.java
+++ src/test/java/stepDefinitions/NavigationStep.java
@@ -1,16 +1,11 @@
 package stepDefinitions;
 
-import io.cucumber.java.AfterStep;
-import io.cucumber.java.Scenario;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -20,13 +15,13 @@ import java.time.Duration;
 public class NavigationStep{
 
     private WebDriver driver;
+    public NavigationStep(BaseTest baseTest){
+        this.driver = baseTest.getDriver();
+    }
 
     @Given("I am on the home page")
     public void i_am_on_the_home_page(){
-        System.out.println("im in step 1");
-        ChromeOptions options =  new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+
         driver.get("https://askomdch.com");
     }
     @When("I click on store menu link")
@@ -40,17 +35,25 @@ public class NavigationStep{
         String expectedUrl = "https://askomdch.com/store/";
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(actualUrl,expectedUrl);
-        driver.quit();
     }
 
-    @AfterStep
-    public void addScreenshot(Scenario scenario){
-
-        //validate if scenario has failed
-        if(scenario.isFailed()) {
-            final byte[] screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES);
-            scenario.attach(screenshot, "image/png", "image");
-        }
-
+    @When("I click on About menu link")
+    public void i_click_on_about_menu_link(){
+        By aboutLink = By.cssSelector("#menu-item-1232>a");
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(aboutLink)).click();
     }
+    @Then("I should see the About page")
+    public void i_should_see_the_about_page(){
+        String expectedUrl = "https://askomdch.com/about/";
+        String actualUrl = driver.getCurrentUrl();
+        Assert.assertEquals(actualUrl,expectedUrl);
+    }
+    @And("I should see the About Us title")
+    public void i_should_see_the_about_us_title(){
+        String expectedTitle = "About Us";
+        String actualTitle = driver.findElement(By.cssSelector("h1[class='has-text-align-center']")).getText();
+        Assert.assertEquals(actualTitle,expectedTitle);
+    }
+
 }
--- src/test/resources/navigation.feature
+++ src/test/resources/navigation.feature
@@ -1,9 +1,15 @@
-Feature: Navigation
+Feature: Navigation Testing
     As a user
     I want to navigate every pages
     So that I can use other features of website
-
-Scenario: Successful navigation to Store Page
+@TC001
+Scenario: Successful navigation to Store Page (TC001)
     Given I am on the home page
     When I click on store menu link
     Then I should see the store page
+@TC002
+Scenario: Successful navigation to About Page (TC002)
+    Given I am on the home page
+    When I click on About menu link
+    Then I should see the About page
+    And I should see the About Us title
